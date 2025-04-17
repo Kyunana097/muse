b65a98a565f546f73313a8c05a97aeaec677fbe7
Kyunana097 <Kyunana097@gmail.com>
2025-04-17 08:58:11 +0800

    RTX51 upload

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
 #include <REGX52.H>
+#include "Headers/LCD1602.h"
+#include "Headers/bmp.h"
+#include "Headers/Delay.h"
+#include "Headers/key.h"
+#include "Headers/oled.h"
+#include "Headers/oledfont.h"
+#include "Headers/Timer0.h"
+#include "Headers/RTX51TNY.H"
 
 void main()
 {
