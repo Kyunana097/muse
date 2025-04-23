8a2ee39fc5927dab8e6dc14dea427b82918e6a13
Kyunana097 <Kyunana097@gmail.com>
2025-04-24 00:24:37 +0800

    note move succeed

--- main.c
+++ main.c
@@ -14,12 +14,13 @@ bit game_state = 0;          // 0: 主菜单，1: 游戏进行中
 bit setting_state = 0;       // 0: 主菜单，1: 设置界面
 bit score_state = 0;         // 0: 主菜单，1: 最高分界面
 volatile unsigned char board_num = 1;           // 1: start 2: setting 3: score
-
-volatile unsigned char speed = 4;
+volatile unsigned char x_position = 119;        //音符起始位置 119+8=127
+volatile unsigned char speed = 4;               //音符移动速度 1-8
 
 // 将 speed 转换为字符串
 char speed_str[2];  // 1位数字 + '\0'
-unsigned char temp ;  // 避免修改原变量
+unsigned char temp ;
+
 unsigned char current_track = 1;//音符列位置 1-2
 
 // 初始化任务
@@ -27,7 +28,7 @@ void task_init(void) _task_ 0
 {
     P2 = 0xFF;
     OLED_Init();
-    OLED_DrawBMP(0, 0, 128, 8, BMP1);
+    OLED_DrawBMP(0, 0, 128, 8, BMP1);//logo
     os_wait(K_IVL, 50, 0);
 
     os_create_task(1);  // 按键任务（优先级1）
@@ -90,22 +91,22 @@ void task_key(void) _task_ 1
                     //game选项下按下确认按键
                     case 1:
                         game_state = 1;
-                        os_delete_task(2);
-                        os_create_task(3);  // 游戏任务（优先级3）
+                        os_delete_task(2);  //挂起菜单
+                        os_create_task(3);  //启动游戏任务（优先级3）
                         break;
                 
                     //setting选项下按下确认按键
                     case 2:
                         setting_state = 1;
-                        os_delete_task(2);
-                        os_create_task(5);  // 设置任务（优先级5）
+                        os_delete_task(2);  //挂起菜单
+                        os_create_task(5);  //启动设置任务（优先级5）
                         break;
 
                     //score选项下按下确认按钮
                     case 3:
                         score_state = 1;
-                        os_delete_task(2);
-                        os_create_task(4);  // 积分任务（优先级4）
+                        os_delete_task(2);  //挂起菜单
+                        os_create_task(4);  //启动积分任务（优先级4）
                         break;
                     }
                     while (KEY3 == 0) os_wait(K_IVL, 1, 0);
@@ -113,12 +114,13 @@ void task_key(void) _task_ 1
                 
             }
             os_wait(K_IVL, 10, 0);      //释放内存
-            //主菜单下不需要按键4
         }
 
         /*游戏中
-        * key1, key2 = sky
-        * key3, key4 = ground
+        * key1 = sky
+        * key2 = ground
+        * key3 = quit
+        * key4 = pause
         */
         if (game_state == 1)
         {
@@ -244,7 +246,7 @@ void task_board(void) _task_ 2
                 OLED_ShowString(3, 6, ">>", 16);
                 break;
             }
-            os_wait(K_IVL, 10, 0);  // 优化刷新率
+            os_wait(K_IVL, 10, 0);  
         }
     }
 }
@@ -264,11 +266,12 @@ void task_game(void) _task_ 3
             {
                 OLED_ShowString(3, 3, "gamestate now", 16); 
             }
+            //game_state == 0后 进入退出步骤
             OLED_Clear();
             os_wait(K_IVL, 100, 0);
             game_state = 0;
-            os_create_task(2);  // 菜单任务（优先级2）
-            os_delete_task(3);
+            os_create_task(2);  //激活菜单
+            os_delete_task(3);  //挂起游戏
         }
         else
         {
@@ -296,11 +299,11 @@ void task_score(void) _task_ 4
                 
                 os_wait(K_IVL, 20, 0);
             }
-
+            //score_state == 0后 进入退出步骤
             os_wait(K_IVL, 100, 0);
             OLED_Clear();
-            os_create_task(2);  // 菜单任务（优先级2）
-            os_delete_task(4);
+            os_create_task(2);  //激活菜单
+            os_delete_task(4);  //挂起积分
         }
         else
         {
@@ -320,26 +323,42 @@ void task_setting(void) _task_ 5
             OLED_Clear();
             os_wait(K_IVL, 10, 0);
             P2 = 0x03;
-            OLED_ShowString(120, 0, "Q", 16);
             while(setting_state == 1)
             {
                 temp = speed;
-                os_wait(K_IVL, 10, 0);
-                speed_str[0] = temp % 10 + '0';          // 个位
+                os_wait(K_IVL, 1, 0);
+
+                //showstring需要一个U8参数 而speed无法直接输出
+                speed_str[0] = temp % 10 + '0';          // 得到speed的值
                 speed_str[1] = '\0';                     // 字符串结束符
-                os_wait(K_IVL, 10, 0);
+
+                os_wait(K_IVL, 1, 0);
                 OLED_ShowString(3, 3, "speed:", 16);
-                os_wait(K_IVL, 10, 0);
+                os_wait(K_IVL, 1, 0);
                 
                 // 传递字符串指针给 OLED_ShowString
-                OLED_ShowString(60, 3, speed_str, 16);  // 强制转换类型
-                os_wait(K_IVL, 20, 0);
+                OLED_ShowString(60, 3, speed_str, 16);  // 强制转换类型 输出当前speed值
+
+                //移动Q（避免使用for而无法识别speed输入）
+                if (x_position > speed && x_position < 120)
+                {
+                    OLED_ShowString(x_position, 0, " ", 16);
+                    x_position = x_position - speed;
+                    OLED_ShowString(x_position, 0, "Q", 16);
+                }
+                else 
+                {
+                    OLED_ShowString(x_position, 0, " ", 16);
+                    x_position = 119;
+                }
+                os_wait(K_IVL, 1, 0);
             }
+
+            //setting_state == 0后 进入退出步骤
             os_wait(K_IVL, 100, 0);
             OLED_Clear();
-            os_create_task(2);  // 菜单任务（优先级2）
-            os_delete_task(5);
-            
+            os_create_task(2);  //激活菜单
+            os_delete_task(5);  //挂起设置
         }
         else
         {
@@ -348,7 +367,7 @@ void task_setting(void) _task_ 5
     }
 }
 
-//蜂鸣器任务
+//蜂鸣器任务（未启用 需使用定时器1）
 void task_beep(void) _task_ 6
 {
     while (1)
